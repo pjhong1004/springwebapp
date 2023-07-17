e913bac7f0893cab6a4a7b56314c08b903c4ad3d
pjhong1004 <pgc1004@gmail.com>
2023-07-17 15:12:24 +0900

    최초 프로젝트 공유 파일

---
+++ src/main/java/com/mycompany/springwebapp/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.mycompany.springwebapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+public class HomeController {
+	//http://localhost:8080/springwebapp/ 생략
+	@RequestMapping("/")
+	public String index() {
+		return "index";
+	}
+}
